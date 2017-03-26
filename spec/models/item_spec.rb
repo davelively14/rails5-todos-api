541992b6dd10b194ebddca0a57d0a7949a96cc64
Dave Lively <davelively14@gmail.com>
2017-03-26 12:38:23 -0400

    Added association and validation for Item and Todo

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -1,5 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe Item, type: :model do
-  
+  # Association tests
+
+  it { should belong_to(:todo) }
+
+  # Validation tests
+
+  it { should validate_presence_of(:name) }
 end
--- spec/models/todo_spec.rb
+++ spec/models/todo_spec.rb
@@ -1,5 +1,13 @@
 require 'rails_helper'
 
 RSpec.describe Todo, type: :model do
-  
+
+  # Association tests
+
+  it { should have_many(:items).dependent(:destroy) }
+
+  # Validation tests
+
+  it { should validate_presence_of(:title) }
+  it { should validate_presence_of(:created_by) }
 end
